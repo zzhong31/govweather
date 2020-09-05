031a01cdfe4031da3a0536cc21b04ad958fc6a6d
zzhong31 <genericname001@gmail.com>
2020-09-05 09:31:48 -0500

    add in heroku environment variables

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
-const apiKeys = require('./config/config')
-const cors = require('cors');
+//const apiKeys = require('./config/config')
+//const cors = require('cors');
+const aws = require('aws-sdk');
+
+let apiKeys = new aws.S3({
+    ipInfoKey: process.env.IPINFO_KEY
+})
 
 const app = express();
-app.use(cors());
+//app.use(cors());
 
 app.get('/api/location/', async (req, res) => {
 
     let result = {};
-    const { data } = await axios.get("https://ipinfo.io?token=" + apiKeys.ipInfoAPI());
+    const { data } = await axios.get("https://ipinfo.io?token=" + apiKeys.ipInfoKey);
     result = data;
 
     res.status(201).send(result);
